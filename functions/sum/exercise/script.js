ed1e3ab4f33ae73ea5ad1b1929cc82d9c9b5bd42
swathisnayak14 <swathisnayak.is19@sahydri.edu.in>
2023-02-04 11:49:32 +0530

    learnt arrays, objects

--- functions/sum/exercise/script.js
+++ functions/sum/exercise/script.js
@@ -8,23 +8,23 @@
   ES6 Syntax (Arrow function): const add = () => {}
 */
 
-function add(){
-  //Add function here
+const add= (a,b)=> a+b
+
+const sub= (a,b)=> a-b
+
+const div= (a,b)=>{
+  if(b==0){
+    console.log("infinity");
+  }
+  else{
+    console.log(a/b);
+  }
 }
 
-function sub(){
-  //Subtract function here
-}
+const mul= (a,b)=> a*b
 
-function div(){
-  //Divide function here
-}
-
-function mul(){
-  //Multiply function here
-}
-
-console.log('hello from the SUM exercise')
-/* 
-  TODO: create a function that console logs the result of any of the above operations.
-*/
+console.log(add(10,20));
+console.log(sub(20,10));
+div(20,10);
+div(10,0);
+console.log(mul(10,20))
--- yourPlayground.js
+++ yourPlayground.js
@@ -1,11 +1,11 @@
 // console.log('hello world')
+//--------------------------------------------
 
 // variables(var,const,let)
 
 // let name='Swathi'
 // console.log(name)
-
-
+//--------------------------------------------
 
 //operators
 
@@ -13,11 +13,11 @@
 // tipPercentage=0.2
 // tipAmount=food*tipPercentage
 // console.log(tipAmount)
-
+//--------------------------------------------
 
 //FIRST JS APP
 
-const prompt = require ("prompt-sync")();
+const prompt = require("prompt-sync")();
 // //user input
 // food=Number(prompt('How much was the tip amount? '))
 // tipPercentage=Number(prompt('tip % ')/100)
@@ -25,7 +25,7 @@ const prompt = require ("prompt-sync")();
 // console.log('Tip Amount: ',tipAmount)
 // total=food+tipAmount
 // console.log('Total Amount: ',total)
-
+//-------------------------------------------------
 
 //data types
 
@@ -34,7 +34,7 @@ const prompt = require ("prompt-sync")();
 //arrays:[]
 //objects:{}
 //boolean:true or false
-
+//-------------------------------------------------
 
 //Math Operators
 
@@ -44,7 +44,7 @@ const prompt = require ("prompt-sync")();
 //Modulo/Remainder % 5 % 2=1
 //Add +
 //Subtract -
-
+//-------------------------------------------------
 
 //Math Methods
 
@@ -53,7 +53,7 @@ const prompt = require ("prompt-sync")();
 //Random: gives you number between 0 and 1
 
 // console.log(Math.floor(Math.random()*3))
-
+//-------------------------------------------------
 
 //SECOND JS APP
 //BUILD A WEATHER APP (conditionals(==,===,<,>))
@@ -69,12 +69,13 @@ const prompt = require ("prompt-sync")();
 // console.log('wear your sunglasses')
 // }
 
+//------------------------------------------------
 //Functions
 
 //it has 0 arguments
 //does: it logs out name to the console
-function sayMyName(){
-    console.log('Swathi')
+function sayMyName() {
+  console.log("Swathi");
 }
 
 //sayMyName()
@@ -82,47 +83,140 @@ function sayMyName(){
 //Dynamic Functions
 
 //it has 1 arguments
-//does: it logs out name to the console 
-function sayMyName2(name){
-    console.log(name)
+//does: it logs out name to the console
+function sayMyName2(name) {
+  console.log(name);
 }
 
 //sayMyName2('Hey there!')
 
-
 //it has 1 arguments
-//does: it logs out name to the console 
-function greeting(name){
-    greet='hi ' + name+',nice to meet you'
-    console.log(greet)
+//does: it logs out name to the console
+function greeting(name) {
+  greet = "hi " + name + ",nice to meet you";
+  console.log(greet);
 }
 
 //greeting('Lance')
 
 //using template literals to avoid concatenations
 
-function greeting(name){
-    //template literals
-    greet=`hi ${name}, Nice to meet you!`
+function greeting(name) {
+  //template literals
+  greet = `hi ${name}, Nice to meet you!`;
 
-    console.log(greet)
+  console.log(greet);
 }
 //greeting('Lance')
 
-function sum(a,b){
-    //return
+function sum(a, b) {
+  //return
 
-    return a+b
+  return a + b;
 }
 
 // num=sum(1,2)
 // console.log(num)
 
-function foodTotal(food,tip){
-    tipPercentage=Number(tip/100)
-    tipAmount=food*tipPercentage    
-    total=sum(food,tip)
-    return total
+function foodTotal(food, tip) {
+  const tipPercentage = Number(tip / 100);
+  const tipAmount = food * tipPercentage;
+  const total = sum(food, tip);
+  return total;
 }
 
 //console.log(foodTotal(100,20))
+//-------------------------------------------------
+
+//ES6 way
+//arrow  function (=>)
+//arrow  function with explicit return
+const sumArrow = (a,b)=>{
+    return a+b
+}
+
+//arrow  function with implicit return(doesn't have squigglies)
+const summArrow2 =(a,b)=> a+b
+
+//console.log(summArrow2(30,50))
+//--------------------------------------------------
+
+//ARRAYS
+
+const groceries=["banana","apple","orange","pear"]
+// console.log(groceries)
+// console.log(groceries[0])
+// console.log(groceries[3])
+
+//ARRAY METHODS
+//1.push
+groceries.push('cherry')
+//console.log(groceries)
+
+//2.slice
+//start from 0 inclusive upto 2 but ot 2)
+//console.log(groceries.slice(0,2))
+
+//console.log(groceries.slice(2,6))
+//console.log(groceries.slice(1,4))
+
+//3.indexof
+//gives the indexx of the desired
+//console.log(groceries.indexOf("orange"))
+//console.log(groceries.indexOf("pear"))
+
+//4.length
+//console.log(groceries.length)
+//-------------------------------------------------
+
+//OBJECTS{}=key-value pairs
+//here person is the object
+const person={
+    name:"Leonardo",
+    shirt:"white"
+}
+//dot notation of accessing objects
+// console.log(person.name)
+// console.log(person.shirt)
+
+//bracket notation of accessing objects
+// console.log(person['name'])
+// console.log(person['shirt'])
+
+
+//assigning object
+person.phone='6715245561'
+//console.log(person.phone)
+
+
+//console.log(person)
+
+const person2={
+    name:"Qazi",
+    shirt:"black"
+}
+
+// console.log(person2.name)
+// console.log(person2.shirt)
+// console.log(person2)
+
+
+//doing using function
+//template literals
+//ES6 way
+//objects
+
+const introducer=(name,shirt) =>{
+    const person3={
+        name:name,
+        shirt:shirt,
+        assets:100000,
+        liabilities:50000
+    }
+    const intro= `Hi! My name is ${person3.name} and the color of my shirt is ${person3.shirt} and my net worth is ${person3.assets-person3.liabilities} USD`
+
+    return intro
+}
+
+//console.log(introducer('Swathi','blue'))
+console.log(introducer("Leo","white",))
