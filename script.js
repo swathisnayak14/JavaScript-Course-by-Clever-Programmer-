2c849643bc528635a55763171dd2c775f6fce865
swathisnayak14 <swathisnayak.is19@sahydri.edu.in>
2023-02-13 12:22:07 +0530

    Tip Calculator

--- script.js
+++ script.js
@@ -44,25 +44,29 @@
 
 //telling the number of times the squares were clicked
 
-const squares=document.querySelectorAll('.colorSquare')
-const timesClicked={'red':0,'yellow':0,'green':0}
-squares.forEach(square=>{
-    square.onclick=()=>{
-        timesClicked[square.value]+=1
-        square.innerText=timesClicked[square.value]
-    }
-})
+// const squares=document.querySelectorAll('.colorSquare')
+// const timesClicked={'red':0,'yellow':0,'green':0}
+// squares.forEach(square=>{
+//     square.onclick=()=>{
+//         timesClicked[square.value]+=1
+//         square.innerText=timesClicked[square.value]
+//     }
+// })
 
 //to clear the numbers 
 
-const clearGameBtn=document.getElementById('clear-game')
-function clearScores(){
-    timesClicked.red=0
-    timesClicked.yellow=0
-    timesClicked.green=0
-    squares.forEach(square=>{
-        square.innerText=''
-    })
-}
-clearGameBtn.onclick=()=>clearScores()
+// const clearGameBtn=document.getElementById('clear-game')
+// function clearScores(){
+//     timesClicked.red=0
+//     timesClicked.yellow=0
+//     timesClicked.green=0
+//     squares.forEach(square=>{
+//         square.innerText=''
+//     })
+// }
+// clearGameBtn.onclick=()=>clearScores()
+
+//-----------------------------------------------------------------
+
+
 
